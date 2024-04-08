810e1a8b5a3b733d44d37d31a5feacbaa45d73fa
juhair-cupcake <juhair98@pm.me>
2024-04-08 14:42:50 +0600

    Update random

--- hello/hello.js
+++ hello/hello.js
@@ -1,7 +1,57 @@
+const EventEmitter = require("events");
+
+const subject = new EventEmitter();
 const terminalInput = process.argv[2];
 
-if (terminalInput === "hello") {
-  console.log("hi");
+if (terminalInput === "hi") {
+  console.log("hello");
 } else {
-  console.log("unknown command");
+  console.log("...");
 }
+
+//all event observer
+process.on('exit', (code) => {
+  console.log('all events closed', code);
+});
+
+// observer 1
+subject.on("event", (result) => {
+  if (result === "pos") {
+    console.log("yeeee");
+  } else if (result === "neg") {
+    console.log("yeet");
+  }
+});
+// observer 2
+subject.on("event", (result) => {
+  if (result === "pos") {
+    console.log("ok");
+  } else if (result === "neg") {
+    console.log("no");
+  }
+});
+
+subject.emit("event", "pos");
+subject.emit("event", "neg");
+
+/*
+how node work?
+
+[
+  V8,
+  node-api [fs, https, path...]
+  node-bindings,
+  libuv
+]
+*/
+
+/*
+libuv and node multi-threading
+
+[
+  event-loop,
+  thread-pool,
+  async-i/o [filesystem, network],
+]
+
+*/
